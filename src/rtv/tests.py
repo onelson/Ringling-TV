bc1f5dd9aa25f5130f6aff79757d56e5b7750fee
Owen Nelson <onelson@gmail.com>
2010-05-13 17:46:52 +0000

    updating unittest to use new fedora module

--- src/rtv/tests.py
+++ src/rtv/tests.py
@@ -1,15 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from fcrepo.connection import Connection
-from fcrepo.client import FedoraClient
+import rtv.fedora
 
 class FedoraServerTest(TestCase):
     fixtures = ['test_users']
     def setUp(self):
-        self.connection = Connection('http://localhost:8080/fedora',
-            username='fedoraAdmin',
-            password='fedoraAdmin')
         self.user = User.objects.get(username='brian')
         
     def testCanTalkToFedoraServer(self):
-        client = FedoraClient(self.connection)
+        client = rtv.fedora.get_client()
