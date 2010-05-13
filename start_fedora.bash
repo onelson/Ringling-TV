36b0b311c15a636fda45aac21ead309dcc74aab5
Owen Nelson <onelson@gmail.com>
2010-05-13 17:13:04 +0000

    trying to get the build system to work -- fedora server is not launching correctly

--- start_fedora.bash
+++ start_fedora.bash
@@ -3,8 +3,6 @@
 SCRIPT=`which $0`
 echo $SCRIPT
 DIR=`dirname $SCRIPT`
-FEDORA_HOME=$DIR/parts/fedora
-CATALINA_HOME=$FEDORA_HOME/tomcat
-echo "FEDORA_HOME: $FEDORA_HOME"
-echo "CATALINA_HOME: $CATALINA_HOME"
-$CATALINA_HOME/bin/startup.sh
+export FEDORA_HOME="$DIR/parts/fedora" && \
+export CATALINA_HOME="$FEDORA_HOME/tomcat" && \
+sh "$CATALINA_HOME/bin/catalina.sh" start           '
