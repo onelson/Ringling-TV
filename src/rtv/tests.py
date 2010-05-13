fcc4f807e313e2a58f450c8c2ddec1811ae5de40
Owen Nelson <onelson@gmail.com>
2010-05-13 18:01:52 +0000

    adding unittests for the video model

--- src/rtv/tests.py
+++ src/rtv/tests.py
@@ -9,3 +9,17 @@ class FedoraServerTest(TestCase):
         
     def testCanTalkToFedoraServer(self):
         client = rtv.fedora.get_client()
+
+class VideoModelTest(TestCase):
+    fixtures = ['test_users']
+    def setUp(self):
+        self.user = User.objects.get(username='meg')
+
+    def testCanCreate(self):
+        raise NotImplementedError
+    def testCanTranscode(self):
+        raise NotImplementedError
+    def testCanSendToFedora(self):
+        raise NotImplementedError
+    def testCanDelete(self):
+        raise NotImplementedError
