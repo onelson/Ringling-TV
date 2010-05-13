a873bdfcd0799f7204e32a0dc8c56e09c013ae02
Owen Nelson <onelson@gmail.com>
2010-05-13 17:23:13 +0000

    updated start/stop fedora scripts

--- start_fedora.bash
+++ start_fedora.bash
@@ -3,6 +3,4 @@
 SCRIPT=`which $0`
 echo $SCRIPT
 DIR=`dirname $SCRIPT`
-export FEDORA_HOME="$DIR/parts/fedora" && \
-export CATALINA_HOME="$FEDORA_HOME/tomcat" && \
-sh "$CATALINA_HOME/bin/catalina.sh" start           '
+export FEDORA_HOME="$DIR/parts/fedora" && export CATALINA_HOME="$FEDORA_HOME/tomcat" && sh "$CATALINA_HOME/bin/catalina.sh" start
