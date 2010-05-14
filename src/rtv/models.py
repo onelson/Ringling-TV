9bd4900aa098fb8362ce51059ac7e096d5fece03
Owen Nelson <onelson@gmail.com>
2010-05-14 19:30:20 +0000

    refs #323 fixed upload process (no handler required), removed some cruft from the video model, and corrects some validation aspects (blank allowed) of it as well.

--- src/rtv/models.py
+++ src/rtv/models.py
@@ -20,79 +20,36 @@ class Video(models.Model):
     video = models.FileField(upload_to='uploads', editable=True)                             #the uploaded video
     video_flv = models.FileField(upload_to='processed_videos', editable=False, null=True)    #the processed video
     uploaded_date = models.DateTimeField(auto_now_add=True, editable=False, null=False)      #datetime the initial save() was called for this video
-    title = models.CharField(max_length=100, editable=True)                                                 #video title
-    video = models.FileField(upload_to='uploads', editable=True)                                  #the uploaded video
+    title = models.CharField(max_length=100)                                                 #video title
+    video = models.FileField(upload_to='uploads')                                  #the uploaded video
     video_flv = models.FileField(upload_to='processed_videos', editable=False, 
         null=True)                                  #the processed video
     uploaded_date = models.DateTimeField(auto_now_add=True, editable=False, 
         null=False)      #datetime the initial save() was called for this video
     process_start = models.DateTimeField(editable=False, null=True)                          #datetime status was set to 'processing'
     process_end = models.DateTimeField(editable=False, null=True)                            #datetime status was set to 'processed'
-    status = models.IntegerField(editable=False, null=False)                        #status code
-    #DC Metadata Fields
-    DCtitle = models.CharField(max_length=100, editable=True, null=True)
-    DCcreator = models.CharField(max_length=100, editable=False, null=False)
-    DCsubject = models.CharField(max_length=100, editable=True, null=True)
-    DCdescription = models.TextField(max_length=500, editable=True, null=True)
-    DCpublisher = models.CharField(max_length=100, editable=True, null=True)
-    DCcontributor = models.CharField(max_length=100, editable=True, null=True)
-    DCdate = models.DateTimeField(editable=True, null=True)
-    DCtype = models.CharField(max_length=50, editable=True, null=True)
-    DCformat = models.CharField(max_length=25, editable=True, null=True)
-    DCidentifier = models.CharField(max_length=50, editable=True, null=True)
-    DCsource = models.CharField(max_length=50, editable=True, null=True)
-    DClanguage = models.CharField(max_length=50, editable=True, null=True)
-    DCrelation = models.CharField(max_length=50, editable=True, null=True)
-    DCcoverage = models.CharField(max_length=50, editable=True, null=True)
-    DCrights = models.TextField(max_length=800, editable=True, null=True)
     status = models.SmallIntegerField(choices=STATUS_CHOICES, 
     default=STATUS_PENDING, editable=False)    #status code
+    #DC Metadata Fields
+    DCsubject = models.CharField(max_length=100, blank=True, null=True)
+    DCdescription = models.TextField(max_length=500, blank=True, null=True)
+    DCpublisher = models.CharField(max_length=100, blank=True, null=True)
+    DCcontributor = models.CharField(max_length=100, blank=True, null=True)
+    DCdate = models.DateTimeField(blank=True, null=True)
+    DCtype = models.CharField(max_length=50, blank=True, null=True)
+    DCformat = models.CharField(max_length=25, blank=True, null=True)
+    DCidentifier = models.CharField(max_length=50, blank=True, null=True)
+    DCsource = models.CharField(max_length=50, blank=True, null=True)
+    DClanguage = models.CharField(max_length=50, blank=True, null=True)
+    DCrelation = models.CharField(max_length=50, blank=True, null=True)
+    DCcoverage = models.CharField(max_length=50, blank=True, null=True)
+    DCrights = models.TextField(max_length=800, blank=True, null=True)
     
+    
+    @staticmethod
     def get_new_form(*args, **kwargs):
         return NewVideoForm(*args, **kwargs)
     
 class NewVideoForm(ModelForm):
     class Meta:
         model = Video
-        fields = (
-                  'user',
-                  'title',
-                  'video',
-                  'video_flv',
-                  'uploaded_date',
-                  'process_start',
-                  'process_end',
-                  'status'
-                  )
-        
-class FileUploadHandler(object):
-    def new_file(self, content_type):
-        self.content_type = 'video/x-msvideo', 'video/x-dv',  
-        self.content_type = 'video/x-dv'
-        self.content_type = 'video/vnd.mpegurl'
-        self.content_type = 'video/x-m4v'
-        self.content_type = 'video/quicktime'
-        self.content_type = 'video/x-sgi-movie'
-        self.content_type = 'video/mp4'
-        self.content_type = 'video/mpeg'
-    def recieve_data_chunk(self, raw_data, start):
-        raise NotImplementedError()
-    def file_complete(self, file_size):
-        raise NotImplementedError()
-    def upload_complete(self):
-        pass
-    
-class UploadFileForm(models.FileField):
-    def handle_uploaded_file(f, instance):
-        instance.field.save('name_slug.ext', f, True)
-        instance.save()
-    def upload_file(self, request):
-        if request.method == 'POST':
-            form = UploadFileForm(request.POST, request.FILES)
-            if form.is_valid():
-                    UploadFileForm.handle_uploaded_file(request.FILES['file'])
-                    return HttpResponseRedirect('/success/url/')
-        else:
-            form = UploadFileForm()
-        return render_to_response('upload.html', {'form': form})
-        
--- src/rtv/views.py
+++ src/rtv/views.py
@@ -1,22 +1,26 @@
 import rtv
-from rtv.models import Video, UploadFileForm
+from rtv.models import Video
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django.contrib.auth.models import User
 #make sure to set <form enctype="multipart/form-data">
-def upload_form(request):
+def upload(request):
+    user = list(User.objects.all())[0]
     form = Video.get_new_form()
+    context = {
+        'rtv_version': rtv.get_version(),
+        'title': "This is the rtv demo page",
+        'form': form }
     if request.method == 'POST':
         form = Video.get_new_form(request.POST, request.FILES)
+        context['form'] = form
         if form.is_valid():
             vals = form.cleaned_data
-            dict = Video.objects.create(**vals)
-            UploadFileForm.handle_uploaded_file(request.FILES['file'])
-            return render_to_response('rtv/success.html', mimetype="application/octet-stream")
-        #no else
-    else:
-        form = Video.get_new_form()
-        return render_to_response('rtv/upload.html', {'form': form},RequestContext(request))
+            vals['user'] = user
+            Video.objects.create(**vals)
+            context['title'] = "This is the rtv success page" 
+            return render_to_response('rtv/success.html', context, RequestContext(request))
+    return render_to_response('rtv/upload.html', context , RequestContext(request))
             
             
 def demo(request):
