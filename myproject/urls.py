470f03c74d66b4c5cf1fcf807e25b4f8de8ff147
Owen Nelson <onelson@gmail.com>
2010-05-14 17:39:09 +0000

    refs #325 adding a redirect from / to /rtv

--- myproject/urls.py
+++ myproject/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns(
     (r'^admin/(.*)', admin.site.root),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^rtv/', include('rtv.urls')),
+    (r'^$', 'django.views.generic.simple.redirect_to', {'url':'/rtv/'})
 )
 
 if settings.SERVE_STATIC_MEDIA:
