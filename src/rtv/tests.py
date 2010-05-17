af302aae2b464a56b275def57e5e205062c94c8f
Owen Nelson <onelson@gmail.com>
2010-05-17 05:21:31 +0000

    updating tests to reflect new api

--- src/rtv/tests.py
+++ src/rtv/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.core.files import File
 from django.contrib.auth.models import User
-import rtv.fedora
-from rtv.models import Video
+from rtv.fedora import get_client
+from rtv.models import TranscodeJob
 import tempfile, os
 
 class FedoraServerTest(TestCase):
@@ -10,9 +11,9 @@ class FedoraServerTest(TestCase):
         self.user = User.objects.get(username='brian')
         
     def testCanTalkToFedoraServer(self):
-        client = rtv.fedora.get_client()
+        client = get_client()
 
-class VideoModelTest(TestCase):
+class TranscodeJobTest(TestCase):
     fixtures = ['test_users']
     
     def createFakeVid(self):
@@ -23,23 +24,26 @@ class VideoModelTest(TestCase):
         vid_file = tempfile.NamedTemporaryFile(prefix='rtvtest', delete=False)
         vid_file.write("I'm a video " * (1024**2))
         vid_file.close()
-        from django.core.files import File
         vfh = File(open(vid_file.name,'rb'))
         self.created_vid = {'size':os.path.getsize(vfh.name)}
-        vid = Video.objects.create(user=self.user, title='test video', 
-            video=vfh)
+        vid = TranscodeJob.objects.create(user=self.user, title='test video')
+        vid.source.save('tmp',vfh, save=True)
+        self.tmpfile = vid_file.name
         self.created_vid['pk'] = vid.pk
-        self.created_vid['path'] = vid.video.path
+        self.created_vid['path'] = vid.source.path
     
     def setUp(self):
+        self.tmpfile = None
         self.user = User.objects.get(username='meg')
         self.createFakeVid()
+    def tearDown(self):
+        os.remove(self.tmpfile)
 
     def testCanCreate(self):
         self.assertTrue(os.path.exists(self.created_vid['path']))
     def testCanGet(self):
-        obj = Video.objects.get(pk=self.created_vid['pk'])
-        self.assertEqual(os.path.getsize(obj.video.path), self.created_vid['size'])
+        obj = TranscodeJob.objects.get(pk=self.created_vid['pk'])
+        self.assertEqual(os.path.getsize(obj.source.path), self.created_vid['size'])
     
     def testCanTranscode(self):
         raise NotImplementedError
@@ -47,6 +51,6 @@ class VideoModelTest(TestCase):
         raise NotImplementedError
     
     def testCanDelete(self):
-        obj = Video.objects.get(pk=self.created_vid['pk'])
+        obj = TranscodeJob.objects.get(pk=self.created_vid['pk'])
         obj.delete()
         self.assertFalse(os.path.exists(self.created_vid['path']))
