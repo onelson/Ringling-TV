d2f040776135d6c2d88535bd21b9e320c4b2f5f7
Owen Nelson <onelson@gmail.com>
2010-05-18 15:11:01 +0000

    updaing docs script and config, but I don't think it's helping anything

--- build-docs.bash
+++ build-docs.bash
@@ -1,9 +1,5 @@
-START_DIR=`pwd`
-ROOT=`which $0`
-ROOT=`dirname $ROOT`
-PATH=$ROOT/bin:$PATH
-DOCS=$ROOT/docs
+SCRIPT=`which $0`
+ROOT=`dirname $SCRIPT`
 export DJANGO_SETTINGS_MODULE=myproject.local_settings
-echo "Building docs in [$DOCS]"
-sphinx-build -b html -d $DOCS/_build/doctrees $DOCS $DOCS/_build/html
-echo 'Done.'
+PATH=$ROOT/bin:$PATH
+$ROOT/bin/sphinx-build -b html -d $ROOT/docs/_build/doctrees   $ROOT/docs/ $ROOT/docs/_build/html
